d35bafddccfad8e9bbddf9e91135b5bcbc7f84ce
nicolaiandersson <nico602d@stud.kea.dk>
2023-04-26 23:54:01 +0200

    -

---
+++ o3/src/Main.java
@@ -0,0 +1,30 @@
+/*
+Øvelse 3
+1. Opret en ArrayList af heltal (Integer) og tilføj 10 tilfældige tal til listen.
+2. Lav en metode "findMinOgMax" for at finde minimum og maksimum værdier i listen.
+3. Udskriv minimum, maksimum og gennemsnit af tal i listen.
+ */
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public int findMinOgMax() {
+        return 0;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> heltal = new ArrayList<Integer>();
+        heltal.add(2);
+        heltal.add(4);
+        heltal.add(6);
+        heltal.add(8);
+        heltal.add(10);
+        heltal.add(12);
+        heltal.add(14);
+        heltal.add(16);
+        heltal.add(18);
+        heltal.add(20);
+    }
+
+
+}
